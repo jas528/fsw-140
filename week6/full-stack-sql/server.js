a1075e1b31f7cb4f48120b485a824ff2a41fddc8
jas528 <76809910+jas528@users.noreply.github.com>
2021-11-09 13:28:48 -0600

    wk6

--- week6/full-stack-sql/server.js
+++ week6/full-stack-sql/server.js
@@ -1,9 +1,39 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const mysql =require('mysql')
+//creates a connection string
 
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'123456789'
+});
+// establish a connection
+db.connect((err)=>{
+    if (err){
+    throw err;
+}
+    console.log ("avengers database!")
+});
+
+//SET UP SERVER
 app.use(morgan('dev'))
 
-app.listen(9000, () => {
-    console.log('The server is listening on port 9000!')
-})
+
+//CREATE A TEST DATABASE
+app.get('/select',(req,res)=>{
+    let sql= "SELECT * FROM avengers";
+
+    //RUN te SQL Command
+    db.query (sql,(err,result)=>{
+        if(err){
+            throw err;}
+            console.log(result);
+            res.send(result)
+        });
+    });
+//OPEN AND LISTEN FROM PORT
+app.listen(4000, () => {
+    console.log('avengers database send sucessfully')
+});
